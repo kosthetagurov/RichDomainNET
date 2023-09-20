4413ed2f67c944865543c020e684320279fdaef1
Константин Хетагуров <hetagurov@mwpost.ru>
2023-09-20 14:37:58 +0300

    Lazy реализация подключения к базе данных

--- RichDomainNET/Abstractions/RichDomainModelContext.cs
+++ RichDomainNET/Abstractions/RichDomainModelContext.cs
@@ -1,14 +1,31 @@
 ﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace RichDomainNET.Abstractions
 {
     public sealed class RichDomainModelContext
     {
-        public IDbConnection Connection { get; }
+        private readonly Lazy<IDbConnection> _dbConnection;
+        public IDbConnection Connection => _dbConnection.Value;
 
         public RichDomainModelContext(IDbConnection connection)
         {
-            Connection = connection;
+            var connectionString = connection.ConnectionString;
+
+            _dbConnection = new Lazy<IDbConnection>(() =>
+            {
+                if (connection != null && connection.State == ConnectionState.Closed)
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    return connection;
+                }
+                else
+                {
+                    throw new Exception("RichDomainModelContext is invalid");
+                }                
+            });
         }
     }
 }
